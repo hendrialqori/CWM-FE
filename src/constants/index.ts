891fe4bef9f57dbcba52c30e66912cb4ab390a92
Hendri Alqori <teamhendri18@gmail.com>
2024-10-14 20:33:12 +0700

    feat: update testimonial section

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,39 +8,49 @@ export const STATIC = CLIENT + "/static"
 
 export const IS_EXPAND_SIDEBAR = false
 
-export const testimonial: Testimony[] = [
+export const testimonial = [
     {
-        image: "/image/cewek-random.jpg",
+        image: "/image/WA-testimoni-1.jpeg",
         message: "Dikira orang lokal gara-gara lancar banget! Dapet diskon ​khusus local di mana-mana!",
         person: { name: "Rini", age: 24, job: "Karyawan swasta" }
     },
     {
-        image: "/image/cewek-random.jpg",
+        image: "/image/WA-testimoni-2.jpeg",
         message: "Bisa nabung 50% budget karena gak perlu guide! Shopping ​jadi lebih banyak, hehe.",
         person: { name: "Dinda", age: 28, job: "Traveler" }
     },
     {
-        image: "/image/cewek-random.jpg",
+        image: "/image/WA-testimoni-3.jpeg",
         message: "Serius, ini ebook paling praktis yang pernah aku coba. Dalam seminggu aja udah bisa ngobrol basic Mandarin! Super puas",
         person: { name: "Andi", age: 25, job: "Mahasiswa" }
     },
     {
-        image: "/image/cewek-random.jpg",
+        image: "/image/WA-testimoni-1.jpeg",
         message: "Aku biasanya bosen kalau belajar bahasa, tapi ebook ini beda banget. Lucu, interaktif, dan ngebantu banget buat liburan ke China. Recommended!",
         person: { name: "Dian", age: 33, job: "Freelancer" }
     },
     {
-        image: "/image/cewek-random.jpg",
+        image: "/image/WA-testimoni-2.jpeg",
+        message: "Gak nyangka bisa ngobrol Mandarin secepat ini! Ebook-nya bener-bener gampang dipahami, dan materinya langsung bisa dipakai buat sehari-hari.",
+        person: { name: "Sarah", age: 28, job: "Traveler" }
+    },
+    {
+        image: "/image/WA-testimoni-3.jpeg",
         message: "Gak nyangka bisa ngobrol Mandarin secepat ini! Ebook-nya bener-bener gampang dipahami, dan materinya langsung bisa dipakai buat sehari-hari.",
         person: { name: "Sarah", age: 28, job: "Traveler" }
     }
 ]
 
 export const productKnowledgePoints = [
-    "Check-in di bandara China dengan mudah!",
-    "Percakapan checkin bagasi di bandara China dan memilih tempat duduk di pesawat dalam Bahasa Mandarin!",
-    "Membuka topik percakapan dengan orang China supaya bisa merasakan budaya China lebih dekat lag",
-    "Pesan makanan di restoran dalam Bahasa Mandarin gak bingung lagi!",
-    "Tips and trick cara pesan makanan dalam menu Bahasa Mandarin serta rekomendasi menu masakan ala China yang lagi ngetrend banget!",
-    "Cara berkenalan dengan orang lokal China pakai Bahasa Mandarin!"
+    "Cara check-in bagasi di China dalam Bahasa Mandarin!",
+    "Cara memilih tempat duduk di pesawat~",
+    "Membuka topik percakapan dengan orang China supaya bisa merasakan budaya China lebih dekat lagi~!",
+    "Tips and trick cara pesan makanan dalam menu Bahasa Mandarin",
+    "Rekomendasi menu masakan ala China yang lagi ngetrend banget!",
+    "Cara berkenalan dengan orang lokal China pakai Bahasa Mandarin!",
+    "Cara memesan taxi di aplikasi China",
+    "Cara check-in dan check-out di hotel China",
+    "Belanja online dan offline di China dalam Bahasa Mandarin!",
+    "Cara top Up Alipay & Wechat Pay",
+    "Banyak latihan listening dan mini quiz!"
 ]
--- src/modules/landing-page/testimonial.tsx
+++ src/modules/landing-page/testimonial.tsx
@@ -1,69 +1,47 @@
 import React from "react";
 import Image from "next/image"
-// import green_shape from "#/assets/svg/green-shape.svg?url"
-// import blue_shape from "#/assets/svg/blue-shape.svg?url"
+import green_shape from "#/assets/svg/green-shape.svg?url"
+import blue_shape from "#/assets/svg/blue-shape.svg?url"
 import pink_circle_shape from "#/assets/svg/pink-circle-shape.svg?url"
-import { Testimony as TTestimony } from "#/@type";
 import { testimonial } from "#/constants";
 
-type TestimonyProps = TTestimony & { number: number }
-
-function colorPick(number: number) {
-    if (number % 3 === 0) return "bg-[#ADC482]"
-    else if (number % 2 === 0) return "bg-[#6E9FB8]"
-    else return "bg-[#F07B48]"
-}
-
-function Testimony({ number, image, message, person }: TestimonyProps) {
-    const theme = colorPick(number)
+function Testimony({ image }: { image: string }) {
     return (
-        <figure
-            className={`w-full md:w-[48%] xl:w-[30%] rounded-[30px] relative overflow-hidden px-8 py-6 space-y-3 ${theme}`} style={{ "--tw-bg-opacity": "0.15" } as React.CSSProperties}
-        >
-            <div className="flex justify-between items-end">
-                <Image
-                    src={image}
-                    alt="testimony-image"
-                    width={40}
-                    height={40}
-                    className="size-10 rounded-full object-cover"
-                />
-                <div className={`center-flex size-6 rounded-full ${theme} text-xl text-white`}>
-                    <p className="translate-y-1 font-mulish">“</p>
-                </div>
-            </div>
-            <figcaption className="space-y-3 flex flex-col">
-                <p className="font-mulish text-sm font-semibold">{message}</p>
-                <p className="font-mulish text-xs font-semibold ml-auto mr-0">{person.name}, {person.age} tahun - {person.job}</p>
-            </figcaption>
-        </figure>
+        <div className="relative z-10 rounded-[1.25rem] overflow-hidden w-full md:w-[30%] xl:w-[24%]">
+            <Image
+                src={image}
+                width={210}
+                height={277}
+                className="size-full"
+                alt="testimony"
+            />
+        </div>
     )
-
 }
 
-// function GreenShape() {
-//     return (
-//         <Image
-//             src={green_shape}
-//             className="size-10 md:size-20 lg:size-28 absolute -top-3 md:top-0 -left-0 xl:-left-20"
-//             width={112}
-//             height={112}
-//             alt="green-shape"
-//         />
-//     )
-// }
+function GreenShape() {
+    return (
+        <Image
+            src={green_shape}
+            className="size-10 md:size-20 lg:size-28 absolute -top-3 md:top-0 -left-0 xl:-left-20"
+            width={112}
+            height={112}
+            alt="green-shape"
+        />
+    )
+}
 
-// function BlueShape() {
-//     return (
-//         <Image
-//             src={blue_shape}
-//             className="size-10 md:size-20 lg:size-28 absolute -top-10 md:-top-20 -right-0 xl:-right-20"
-//             width={112}
-//             height={112}
-//             alt="blue-shape"
-//         />
-//     )
-// }
+function BlueShape() {
+    return (
+        <Image
+            src={blue_shape}
+            className="size-10 md:size-20 lg:size-28 absolute -top-10 md:-top-20 -right-0 xl:-right-20"
+            width={112}
+            height={112}
+            alt="blue-shape"
+        />
+    )
+}
 
 function PinkCircleShape() {
     return (
@@ -83,22 +61,16 @@ function Testimonial() {
             className="mt-space_between_section_sm md:mt-space_between_section_md xl:mt-space_between_section"
             aria-label="testimonial">
             <div className="landing-page-container2nd relative">
-                {/* <GreenShape /> */}
-                {/* <BlueShape /> */}
+                <GreenShape />
+                <BlueShape />
             </div>
             <div className="landing-page-container2nd space-y-10 md:space-y-20 py-7 md:py-14 relative">
-                <div className="center-flex flex-col space-y-0 md:space-y-2" aria-label="title">
-                    <h2 className="font-fredoka font-bold text-xl md:text-[2rem]">Apa kata Mereka?</h2>
-                    <p className="font-mulish text-sm md:text-xl font-medium text-[#5D5D5D]">lihat pengalaman yang mereka dapatkan</p>
-                </div>
-                <div className="flex justify-center flex-wrap gap-5">
+                <h2 className="font-fredoka font-bold text-xl md:text-[2rem] text-center">Apa kata Mereka?</h2>
+                <div className="flex justify-center flex-wrap gap-3">
                     {testimonial.map((testimony, i) => (
                         <Testimony
                             key={i}
-                            number={i + 1}
                             image={testimony.image}
-                            message={testimony.message}
-                            person={testimony.person}
                         />
                     ))}
                 </div>
