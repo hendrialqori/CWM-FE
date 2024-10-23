1343ce0678509417d318309bd74126d95534d9e5
Hendri Alqori <teamhendri18@gmail.com>
2024-10-23 20:40:38 +0700

    feat!: add preview image and zip file upload

---
+++ src/modules/dashboard/preview-file.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image"
+import { LuPencil } from "react-icons/lu";
+import { PiFileZipDuotone } from "react-icons/pi";
+import { STATIC } from "#/constants"
+
+type ImagePreviewProps = {
+    image: string, onChangeImage: () => void
+}
+
+export function ImagePreview({ image, onChangeImage }: ImagePreviewProps) {
+    return (
+        <div className="flex items-center gap-3">
+            <div className="relative size-16 shadow-md">
+                <Image
+                    src={`${STATIC}/${image}`}
+                    width={40}
+                    height={40}
+                    className="size-full rounded-lg object-cover"
+                    alt="preview-avatar"
+                />
+                <button
+                    type="button"
+                    className="absolute -bottom-2 -right-2 bg-gray-100 border border-gray-200 rounded-full p-1"
+                    onClick={onChangeImage}
+                >
+                    <LuPencil className="text-base" />
+                </button>
+            </div>
+            <p className="text-xs md:text-[0.8rem] line-clamp-1 w-9/12">{image}</p>
+        </div>
+    )
+}
+
+
+type ZipPreviewProps = {
+    zip: string, onChangeZip: () => void
+}
+
+export function ZipPreview({ zip, onChangeZip }: ZipPreviewProps) {
+    return (
+        <div className="flex items-center gap-3">
+            <div className="relative size-16 center-flex bg-gray-50 shadow-md">
+                <PiFileZipDuotone className="text-2xl" />
+                <button
+                    type="button"
+                    className="absolute -bottom-2 -right-2 bg-gray-100 border border-gray-200 rounded-full p-1"
+                    onClick={onChangeZip}
+                >
+                    <LuPencil className="text-base" />
+                </button>
+            </div>
+            <p className="text-xs md:text-[0.8rem] line-clamp-1 w-9/12">{zip}</p>
+        </div>
+    )
+}
