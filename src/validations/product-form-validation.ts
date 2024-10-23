fabde63d4a240532404b6bcb72fdc2d290548cee
Hendri Alqori <teamhendri18@gmail.com>
2024-10-23 20:39:28 +0700

    refactor(product)!: remove update product scheme

--- src/validations/product-form-validation.ts
+++ src/validations/product-form-validation.ts
@@ -1,13 +1,13 @@
 import { z } from "zod"
 
-export const addProductScheme = z.object({
+export const productFormScheme = z.object({
     image: z.any()
-        .refine((images: FileList) => images[0]?.name, "Required")
+        .refine((images: FileList) => images?.[0]?.name, "Required")
         .refine((images: FileList) => {
             const MAX_IMAGE_SIZE = 3_000_000 //3mb
-            return images[0]?.size <= MAX_IMAGE_SIZE
+            return images?.[0]?.size <= MAX_IMAGE_SIZE
         }, "File size too big")
-        .transform((images: FileList) => images[0])
+        .transform((images: FileList) => images?.[0]).optional()
     ,
     name: z.string().min(1, { message: "Required" }).max(100),
     originalPrice: z.string()
@@ -17,24 +17,11 @@ export const addProductScheme = z.object({
         .min(1, { message: "Required" })
         .transform((price) => price.replaceAll(",", "")),
     isOffer: z.boolean().default(false),
-    link: z.string().min(1, { message: "Required" }),
-    description: z.string()
-})
-
-export const updateProductScheme = z.object({
-    name: z.string().min(1, { message: "Required" }).max(100),
-    originalPrice: z.string()
-        .min(1, { message: "Required" })
-        .transform((price) => price.replaceAll(",", "")),
-    strikeoutPrice: z.string()
-        .min(1, { message: "Required" })
-        .transform((price) => price.replaceAll(",", "")),
-    isOffer: z.boolean().default(false).optional(),
-    link: z.string().min(1, { message: "Required" }),
+    zip: z.any()
+        .refine((images: FileList) => images?.[0]?.name, "Required")
+        .transform((zips: FileList) => zips?.[0]).optional(),
     description: z.string().min(1, { message: "Required" })
 })
 
 
-export type AddProductType = z.infer<typeof addProductScheme>
-
-export type UpdateProductType = z.infer<typeof updateProductScheme>
+export type ProductFormType = z.infer<typeof productFormScheme>
