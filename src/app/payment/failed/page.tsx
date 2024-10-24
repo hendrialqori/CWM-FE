90e30c9754370af5e5130ff98163f9b47ef4525f
Hendri Alqori <teamhendri18@gmail.com>
2024-10-24 20:40:12 +0700

    feat: make modular file on success and failed payment page

--- src/app/payment/failed/page.tsx
+++ src/app/payment/failed/page.tsx
@@ -1,7 +1,5 @@
+import { Content } from "#/modules/failed/content";
+
 export default function Failed() {
-    return (
-        <section className="min-h-dvh w-full center-flex">
-            <p className="font-medium -tracking-wide text-red-500">Payment failed</p>
-        </section>
-    )
+    return <Content />
 }
--- src/app/payment/success/page.tsx
+++ src/app/payment/success/page.tsx
@@ -1,7 +1,5 @@
+import { Content } from "#/modules/success/content";
+
 export default function Success() {
-    return (
-        <section className="min-h-dvh w-full center-flex">
-            <p className="font-medium -tracking-wide text-green-500">Payment Success</p>
-        </section>
-    )
+    return <Content />
 }
